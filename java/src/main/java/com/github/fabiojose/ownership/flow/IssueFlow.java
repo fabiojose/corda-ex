f778665ed8e53b841b67e276808744cecd1e90ec
Fabio José <fabiojose@gmail.com>
2018-09-12 15:05:03 -0300

    Flow impl.

--- java/src/main/java/com/github/fabiojose/ownership/flow/IssueFlow.java
+++ java/src/main/java/com/github/fabiojose/ownership/flow/IssueFlow.java
@@ -1,25 +1,33 @@
 package com.github.fabiojose.ownership.flow;
 
 import com.github.fabiojose.ownership.state.OwnershipState;
+import com.github.fabiojose.ownership.contract.OwnershipContract;
 
 import co.paralleluniverse.fibers.Suspendable;
+import com.google.common.collect.ImmutableList;
 
 import net.corda.core.identity.Party;
 
 import net.corda.core.flows.InitiatingFlow;
 import net.corda.core.flows.FlowLogic;
 import net.corda.core.flows.FlowException;
+import net.corda.core.flows.StartableByRPC;
+import net.corda.core.flows.FinalityFlow;
 
 import net.corda.core.transactions.SignedTransaction;
+import net.corda.core.transactions.TransactionBuilder;
 
 import net.corda.core.utilities.ProgressTracker;
 import net.corda.core.utilities.ProgressTracker.Step;
 
 import net.corda.core.contracts.UniqueIdentifier;
+import net.corda.core.contracts.Command;
+
 
 public final class IssueFlow {
 
   @InitiatingFlow
+  @StartableByRPC
   public static class Initiator extends FlowLogic<SignedTransaction> {
 
     private final Step GENERATING_TRANSACTION = new Step("Generating transaction on new ownership");
@@ -60,7 +68,7 @@ public final class IssueFlow {
       // Get the reference to notary's identity
       final Party notary = getServiceHub().getNetworkMapCache().getNotaryIdentities().get(0);
 
-      // *begin* 1.
+      // 1.
       tracker.setCurrentStep(GENERATING_TRANSACTION);
       
       // Get the reference to my identity
@@ -68,7 +76,28 @@ public final class IssueFlow {
 
       // Instance the state
       final OwnershipState state = new OwnershipState(object, description, value, currency, me, new UniqueIdentifier());
-      return null;
+
+      // Instance the command used in the transaction
+      final Command<OwnershipContract.Commands.Issue> issue = new Command<>(new OwnershipContract.Commands.Issue(), ImmutableList.of(me.getOwningKey()));
+
+      // Create a transaction builder instance to issue the ownership for me
+      final TransactionBuilder txBuilder = new TransactionBuilder(notary)
+                                                 .addOutputState(state, OwnershipContract.OWNERSHIP_CONTRACT_ID)
+                                                 .addCommand(issue);
+      
+      // 2.
+      tracker.setCurrentStep(VERIFYING_TRANSACTION);
+
+      // Execute the contract's logic
+      txBuilder.verify(getServiceHub());
+    
+      // 3.
+      tracker.setCurrentStep(SIGNING_TRANSACTION);
+      final SignedTransaction partSignedTx = getServiceHub().signInitialTransaction(txBuilder);
+
+      // 4.
+      tracker.setCurrentStep(FINALIZING_TRANSACTION);
+      return subFlow(new FinalityFlow(partSignedTx));
     }
   }
 }
