07ed41f857bae1b2fa5738ee3bcba436b2132b9e
Fabio José <fabiojose@gmail.com>
2018-09-12 13:22:26 -0300

    Issue command impl

--- java/src/main/java/com/github/fabiojose/ownership/contract/OwnershipContract.java
+++ java/src/main/java/com/github/fabiojose/ownership/contract/OwnershipContract.java
@@ -1,23 +1,47 @@
 package com.github.fabiojose.ownership.contract;
 
+import com.github.fabiojose.ownership.state.OwnershipState;
+
 import net.corda.core.contracts.CommandData;
 import net.corda.core.contracts.CommandWithParties;
-
 import net.corda.core.contracts.Contract;
 import net.corda.core.transactions.LedgerTransaction;
+import net.corda.core.identity.AbstractParty;
 
 import static net.corda.core.contracts.ContractsDSL.requireSingleCommand;
 import static net.corda.core.contracts.ContractsDSL.requireThat;
 
+import java.util.stream.Collectors;
+import java.util.List;
+
 public class OwnershipContract implements Contract {
   public static final String OWNERSHIP_CONTRACT_ID = "com.github.fabiojose.ownership.contract.OwnershipContract";
+
+  private static final int FIRST = 0;
 
   @Override
   public void verify(LedgerTransaction tx) {
     final CommandWithParties<Commands> command = requireSingleCommand(tx.getCommands(), Commands.class);
 
     if(command.getValue() instanceof Commands.Issue){
+      // Emiting the ownership record
+      requireThat(require -> {
+        require.using("No inputs should be consumed when emitting the ownership", tx.getInputs().isEmpty());
+        require.using("Only one output should be produced", tx.getOutputs().size() == 1);
 
+        // Gets the Output state instance from transaction
+        final OwnershipState out = tx.outputsOfType(OwnershipState.class).get(FIRST);
+
+        // Non zero ownership valure
+        require.using("The value of ownershitp should be greater than zero", out.getValue() > 0.0d);
+
+        // Get participants of transaction
+        List<?> participants = out.getParticipants().stream().map(AbstractParty::getOwningKey).collect(Collectors.toList());
+        require.using("All parties must be signers",
+                     command.getSigners().contains(participants));
+
+        return null;
+      });
     } else if(command.getValue() instanceof Commands.Transfer){
 
     } else {
